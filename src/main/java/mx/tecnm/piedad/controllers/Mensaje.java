49d020c4db88edb47357d8d5182688d62dbd8546
hanniahaide <ghanniahaide@gmail.com>
2022-02-21 08:45:24 -0600

    Primer commit del repo

---
+++ src/main/java/mx/tecnm/piedad/controllers/Mensaje.java
@@ -0,0 +1,38 @@
+package mx.tecnm.piedad.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@RequestMapping("/api/mensajes")
+public class Mensaje {
+	
+	@GetMapping("/hola")
+	public String saludar() {
+		return "Hola WS";		
+	}
+	
+	@GetMapping("/eco")
+	public String eco(@RequestParam String mensaje) {
+		return mensaje + " " + mensaje + " " + mensaje;
+	} 
+	
+	@GetMapping("/saludo")
+	public String saludarUsuario(@RequestParam String usuario, @RequestParam String mensaje) {
+		return usuario + " "  + mensaje;
+	}
+	
+	@GetMapping("/mensaje/{numero}")
+	public String elegirMensaje(@PathVariable int numero) {
+		String mensajes[] = new String[] {"Hoy depositan","Arriba yo, mi apa y la chona","Ya es viernes"};
+		try {
+			return mensajes[numero];
+		} catch(Exception e) {
+			return "Suerte para la proxima";
+		}
+	}
+
+}
